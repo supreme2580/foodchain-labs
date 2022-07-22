e1cd090b3542b8c0e7488a1c3e189bbbe6949d57
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-07-22 08:31:24 +0100

    suprememods

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
     return(
@@ -11,10 +12,16 @@ const Navbar = () => {
                 <div className="flex justify-center flex-1">
                     <div className="p-1.5 bg-orange-300 rounded-lg max-w-lg">
                         <div className="flex justify-center items-center space-x-5">
-                            <div className="bg-lime-500 py-1.5 rounded-lg text-white px-5 hover:cursor-pointer">Home</div>
-                            <div className="py-1.5 px-5 hover:cursor-pointer hover:bg-lime-500 hover:text-white rounded-lg">Partners</div>
-                            <div className="py-1.5 px-5 hover:cursor-pointer hover:bg-lime-500 hover:text-white rounded-lg">About Us</div>
-                            <div className="py-1.5 px-5 hover:cursor-pointer hover:bg-lime-500 hover:text-white rounded-lg">Contact</div>
+                            <Link href="/">
+                                <a><div className="bg-lime-500 py-1.5 rounded-lg text-white px-5 hover:cursor-pointer">Home</div></a>
+                            </Link>
+                                <a><div className="py-1.5 px-5 hover:cursor-pointer hover:bg-lime-500 hover:text-white rounded-lg">Partners</div></a>
+                            <Link href="/">
+                                <a><div className="py-1.5 px-5 hover:cursor-pointer hover:bg-lime-500 hover:text-white rounded-lg">About Us</div></a>
+                            </Link>
+                            <Link href="/">
+                                <a><div className="py-1.5 px-5 hover:cursor-pointer hover:bg-lime-500 hover:text-white rounded-lg">Contact</div></a>
+                            </Link>
                         </div>
                     </div>
                 </div>
