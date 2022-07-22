9c1600c9edafb21ddda67b894f1eb9326da5beb5
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-07-22 08:14:15 +0100

    foodchain-labs

---
+++ components/Hero.js
@@ -0,0 +1,18 @@
+const Hero = () => {
+    return(
+        <div className="ml-52 max-w-4xl space-y-5">
+            <div className="text-6xl font-semibold">We are building the backbone of digital <span className="text-lime-500">agriculture</span></div>
+            <div className="max-w-2xl text-2xl">
+                Foodchain is indexing farm produce across Africa
+
+                to enable African farmers to connect with buyers 
+
+                using web3. We are bringing agricultural harvest 
+
+                right to everyone at their finger tip.
+            </div>
+            <button className="w-full max-w-2xl bg-lime-500 text-white text-2xl p-3 rounded-xl">Join the farming revolution</button>
+        </div>
+    )
+}
+export default Hero
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
+import Head from "next/head"
+import Hero from "../components/Hero"
 import Navbar from "../components/Navbar"
 
 const Home = () => {
   return(
-    <div>
+    <div className="space-y-32">
+      <Head>
+        <title>Foodchain Labs</title>
+        <link rel="icon" src="/vercel.svg" />
+      </Head>
       <Navbar />
+      <Hero />
     </div>
   )
 }
