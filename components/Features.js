2c7d103e50df508854c3fb46a2fb84409a107600
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-08-25 16:29:45 +0100

    supremelabs

--- components/Features.js
+++ components/Features.js
@@ -1,17 +1,73 @@
+import Image from "next/image"
+import Link from "next/link"
+
 const Features = () => {
     return(
-        <div className="flex flex-col items-center justify-center space-y-10">
-            <div className="space-y-5">
-                <div className="text-2xl text-[#252820] font-agrandir text-center">Features</div>
-                <div className="max-w-2xl text-center text-2xl text-[#0D0D0D] font-inter">
-                    Foodchain is indexing farm produce across Africa to enable African farmers to connect with buyers using web3. We are bringing agricultural harvest right to everyone at their finger tip.
+        <div>
+            <h3 className="w-full text-center text-3xl font-semibold text-ash">Features</h3>
+            <div className="flex justify-center space-x-2 mt-4">
+                <div className="w-[300px] h-[335px] bg-white px-2.5 py-8 space-y-2.5 shadow-lg shadow-ash">
+                    <div>
+                        <Image src="/avatar.jpg" width={50} height={50} className="rounded-full" />
+                    </div>
+                    <h4 className="text-ash text-2xl font-semibold">Digital Farmers Market</h4>
+                    <p className="text-base font-normal text-ash">The web3 based indexed farm produce data is collected by foodchain community</p>
+                    <div>
+                        <Link href="/">
+                            <a>
+                                <button className="bg-green w-36 flex items-center space-x-2 justify-center p-1.5 shadow-green shadow-lg">
+                                    <p className="text-white">Learn more</p>
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6" fill="#fff">
+                                            <path fillRule="evenodd" d="M16.72 7.72a.75.75 0 011.06 0l3.75 3.75a.75.75 0 010 1.06l-3.75 3.75a.75.75 0 11-1.06-1.06l2.47-2.47H3a.75.75 0 010-1.5h16.19l-2.47-2.47a.75.75 0 010-1.06z" clipRule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </a>
+                        </Link>
+                    </div>
                 </div>
-            </div>
-            <div className="space-y-5">
-                <div className="text-2xl text-[#252820] font-agrandir text-center">Features</div>
-                <div className="text-center text-2xl text-[#0D0D0D] space-y-2.5 font-inter">
-                    <div>- Digital Farmers Market</div>
-                    <div>- Supply Chain Players - Logistic</div>
+                <div className="w-[300px] h-[370px] bg-green px-5 py-8 space-y-2.5 shadow-lg shadow-ash">
+                    <div>
+                        <Image src="/avatar.jpg" width={50} height={50} className="rounded-full" />
+                    </div>
+                    <h4 className="text-white text-2xl font-semibold">Foundation of Agricultural Knowledge</h4>
+                    <p className="text-base font-normal text-white">The web3 based indexed farm produce data is collected by foodchain community</p>
+                    <div>
+                        <Link href="/">
+                            <a>
+                                <button className="bg-white w-36 flex items-center space-x-2 justify-center p-1.5 shadow-lg shadow-ash">
+                                    <p className="text-ash font-semibold">Learn more</p>
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6" fill="#51493D">
+                                            <path fillRule="evenodd" d="M16.72 7.72a.75.75 0 011.06 0l3.75 3.75a.75.75 0 010 1.06l-3.75 3.75a.75.75 0 11-1.06-1.06l2.47-2.47H3a.75.75 0 010-1.5h16.19l-2.47-2.47a.75.75 0 010-1.06z" clipRule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                <div className="w-[300px] h-[335px] bg-white px-2.5 py-8 space-y-2.5 shadow-lg shadow-ash">
+                    <div>
+                        <Image src="/avatar.jpg" width={50} height={50} className="rounded-full" />
+                    </div>
+                    <h4 className="text-ash text-2xl font-semibold">Digital Farmers Market</h4>
+                    <p className="text-base font-normal">The web3 based indexed farm produce data is collected by foodchain community</p>
+                    <div>
+                        <Link href="/">
+                            <a>
+                                <button className="bg-green w-36 flex items-center space-x-2 justify-center p-1.5 shadow-green shadow-lg">
+                                    <p className="text-white">Learn more</p>
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6" fill="#fff">
+                                            <path fillRule="evenodd" d="M16.72 7.72a.75.75 0 011.06 0l3.75 3.75a.75.75 0 010 1.06l-3.75 3.75a.75.75 0 11-1.06-1.06l2.47-2.47H3a.75.75 0 010-1.5h16.19l-2.47-2.47a.75.75 0 010-1.06z" clipRule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </a>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
